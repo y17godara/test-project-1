77f737cfd5edc979506533cd5230c3f4baea534e
YashGodara <yash17godara@gmail.com>
2022-08-22 00:59:54 +0530

    basic-writing

---
+++ project-webpage/src/webApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; //Imported Scanner Used: Line-9.
+
+public class webApp {
+
+    //Functions
+
+    //Main 
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in); //Scanner Object Created.
+
+
+  
+        sc.close(); //Scanner Object Closed.
+    }
+}
